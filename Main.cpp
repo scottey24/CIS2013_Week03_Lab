31b5bc9b264b13625462457f5da00cdbeb295d39
Scott Culp <scot.r.culp@hotmail.com>
2019-02-06 19:18:40 -0600

    added subtracting

--- Main.cpp
+++ Main.cpp
@@ -8,20 +8,25 @@ int main(){
 	cout<< " Our C++ Calculator\n";
 	
 	while('N' != again){
-		cout << "(A)dd";
+		cout << "(A)dd,(S)ubtract";
 		cin >> op;		
 		
-			cout << " Enter first number to add; ";
+			cout << " Enter first number to add: ";
 			cin >> num1;
-			cout << " Enter you r second numnber: ";
+			cout << " Enter you r second numnber:";
 			cin >> num2;
 			
-			cout << endl << num1 << "+" 
-			<< num2<< "=" << (num1 + num2); << endl;
+			if('A' ==op) || ('a') {	
+				cout << endl << num1 << "+" 
+					<< num2<< "=" << (num1 + num2); << endl;
+	}else
+		cout << endl << num1 << "-"
+			<< num2 << "=" << (num1-num2) << end;
+	}else{
+		cout <<op<< " Is not a valid option..." <<endl
 			
-			
-		cout << " Do you want to use Calculator again (Y/N)?";
-		cin >> again;
+			cout << " Do you want to use Calculator again (Y/N)?";
+				cin >> again;
 	}
 	
 	return 0;
