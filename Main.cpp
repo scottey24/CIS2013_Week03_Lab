2940d16d9ca1e0ea5e98079bb0f0b02384225179
Scott Culp <scot.r.culp@hotmail.com>
2019-02-06 18:40:46 -0600

    class examples

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	char op = 'A';
+	char again = 'Y';
+	cout<< " Our C++ Calculator\n";
+	
+	whie('N' != again){
+		cout << : (A)dd or (S)ubtract?";
+		cin >> op;		
+		cout << " Do you want to use Calculator again (Y/N)?";
+		cin >> again;
+	}
+	
+	return 0;
+	
+	}
