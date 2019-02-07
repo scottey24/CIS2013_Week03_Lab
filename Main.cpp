964381264e185c6b33422b3b70bf0302524af19d
Scott Culp <scot.r.culp@hotmail.com>
2019-02-06 19:09:13 -0600

    actually adding

--- Main.cpp
+++ Main.cpp
@@ -7,9 +7,19 @@ int main(){
 	char again = 'Y';
 	cout<< " Our C++ Calculator\n";
 	
-	whie('N' != again){
-		cout << : (A)dd or (S)ubtract?";
+	while('N' != again){
+		cout << "(A)dd";
 		cin >> op;		
+		
+			cout << " Enter first number to add; ";
+			cin >> num1;
+			cout << " Enter you r second numnber: ";
+			cin >> num2;
+			
+			cout << endl << num1 << "+" 
+			<< num2<< "=" << (num1 + num2); << endl;
+			
+			
 		cout << " Do you want to use Calculator again (Y/N)?";
 		cin >> again;
 	}
