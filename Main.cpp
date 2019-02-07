37f12aa32ce5d1be75093d1df04e48d7955e62d8
Scott Culp <scot.r.culp@hotmail.com>
2019-02-06 19:50:02 -0600

    switches and do while

--- Main.cpp
+++ Main.cpp
@@ -13,32 +13,32 @@ int main(){
 	
 	while('N' != again){
 		cout << "(A)dd,(S)ubtract,(M)ultiply";
-		cin >> op;		
+		cin >> op;	
+
+		switch(op) {
+			case 'a':
+			case 'A':
+				cout << endl << num1 << "+" 
+					<< num2<< "=" << (num1 + num2); << endl;
+					break;
+			case 's':
+			case 'S':
+				cout << endl << num1 << "-"
+					<< num2 << "=" << (num1-num2) << endl;
+			break;
 		
 			cout << " Enter first number:";
 			cin >> num1;
 			cout << " Enter you r second numnber:";
 			cin >> num2;
 			
-			if('A' == op)||('a' == op) {	
-				cout << endl << num1 << "+" 
-					<< num2<< "=" << (num1 + num2); << endl;
-			
-			
-	}else if (('S' == op) || ('s' == op)) {
-		cout << endl << num1 << "-"
-			<< num2 << "=" << (num1-num2) << end;
-			
-			
-	}else if (('M' ==op) || ('m' ==op)) {
-		cout << endl << num1 << "x"
-			<< num2 << "=" << (num1*num2) << end;
-	}else{
-		cout <<op<< " Is not a valid option..." <<endl
+			default
+				cout <<op<< " Is not a valid option..." <<endl
 	}
 			
 	cout << " Do you want to use Calculator again (Y/N)?";
 				cin >> again;
+	}while('n' !=again);
 	}
 	
 	return 0;
